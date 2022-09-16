c9eeb7764ea64ac01a71447d32ae0faba26207ce
raoakif <akifrao@gmail.com>
2022-09-16 06:40:19 +0500

    Add methods to businesses controllers

--- backend/app/controllers/api/v1/businesses_controller.rb
+++ backend/app/controllers/api/v1/businesses_controller.rb
@@ -1,7 +1,15 @@
 class Api::V1::BusinessesController < ApplicationController
   def index
+    businesses = Business.all
+    render json: businesses, status:200
   end
 
   def show
+    business = Business.find_by(id: params[:id])
+    if business
+      render json: business, status:200
+    else
+      render json: {error: "Not Found"}
+    end
   end
 end
